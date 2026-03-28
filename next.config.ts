305d7aab8bb7aaaa38f632b5092c81d4dbc04fd7
Vimalraj <rvimalrajcse@gmail.com>
2026-03-28 12:49:15 +0530

    Configure for static export on Render

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false as any,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
